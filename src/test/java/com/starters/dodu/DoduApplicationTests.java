19714bebd63adb5ab3c6b34861c3aa5d53e221e5
JUNHYUNG CHO <junhyungcho@JUNHYUNGui-MacBookPro.local>
2023-02-11 17:47:13 +0900

    initial commit

---
+++ src/test/java/com/starters/dodu/DoduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starters.dodu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
