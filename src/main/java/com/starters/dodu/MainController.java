efbabf087f0b574a3316eaa9d3901df54cd9850f
Kimsooyeon1212 <rlatndus3818@naver.com>
2023-02-14 14:56:37 +0900

    [fea] thymeleaf layout 적용

---
+++ src/main/java/com/starters/dodu/MainController.java
@@ -0,0 +1,23 @@
+package com.starters.dodu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String first(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("offer")
+    public String offer(){
+        return "offertomentor";
+    }
+}
