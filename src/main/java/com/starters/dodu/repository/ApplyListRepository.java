18577045db81f646020032e3065bb8346ddb26e5
zwundzwzig <amoeba8282@gmail.com>
2023-02-21 22:12:56 +0900

    [refactor] 모든 view 페이지 연동 및 2차 스프린트 flow 구현

--- src/main/java/com/starters/dodu/repository/ApplyListRepository.java
+++ src/main/java/com/starters/dodu/repository/ApplyListRepository.java
@@ -2,11 +2,14 @@ package com.starters.dodu.repository;
 
 import com.starters.dodu.domain.Apply;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
 
 public interface ApplyListRepository extends JpaRepository<Apply, Long> {
   List<Apply> findAllByIdOrderByIndateDesc(Long id);
+
+  List<Apply> findAllByMenteeId(Long id);
+
+  Optional<Apply> findByMentee_IdAndMentor_Id(Long menteeId, Long mentorId);
 }
