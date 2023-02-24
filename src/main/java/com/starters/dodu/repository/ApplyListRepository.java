7ebaddc199bb2f216828d79c2dc8e0de18fbf7e4
zwundzwzig <amoeba8282@gmail.com>
2023-02-24 10:38:12 +0900

    [refactor] apply 수정

--- src/main/java/com/starters/dodu/repository/ApplyListRepository.java
+++ src/main/java/com/starters/dodu/repository/ApplyListRepository.java
@@ -1,10 +1,7 @@
 package com.starters.dodu.repository;
 
 import com.starters.dodu.domain.Apply;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,10 +12,5 @@ public interface ApplyListRepository extends JpaRepository<Apply, Long> {
   List<Apply> findAllByMenteeId(Long id);
 
   Optional<Apply> findByMentee_IdAndMentor_Id(Long menteeId, Long mentorId);
-
-  @Transactional
-  @Modifying
-  @Query("UPDATE Apply a SET a.status = ?1 WHERE a.id = ?2")
-  int setStatusForApply(String status, Long id);
 
 }
